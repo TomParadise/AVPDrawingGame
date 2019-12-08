8ced1168e70605847dcefba6587320e092a8b56c
Tom <Thomas2.Paradise@live.uwe.ac.uk>
2019-12-08 14:51:06 +0000

    both eyes, trail draws on trigger press, made trail a little smoother

---
+++ AVPDemo/Assets/DrawScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawScript : MonoBehaviour
+{
+    public TrailRenderer trail;
+    private bool isActive = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Oculus_CrossPlatform_SecondaryIndexTrigger") != 0)
+        {
+            if(!isActive)
+            {
+                isActive = true;
+                trail.emitting = true;
+            }
+        }
+        else if(isActive)
+        {
+            isActive = false;
+            trail.emitting = false;
+        }
+    }
+}

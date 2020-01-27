977c31c623fc69ac5993c21629f093660deb43a3
Tom Paradise <thomas2.paradise@live.uwe.ac.uk>
2020-01-27 15:01:23 +0000

    added basic word bank stuff

---
+++ Assets/Scripts/WordBank.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordBank : MonoBehaviour
+{
+    public TextAsset wordBank;
+    private string[] dataLines;
+
+    private float timer = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dataLines = wordBank.text.Split('\n');
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0.0f)
+        {
+            timer = 1.0f;
+            Debug.Log(dataLines[Random.Range(0,999)]);
+        }
+    }
+}

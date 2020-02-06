dc11d348ed8a15a68c98e4545aad84c965804d1e
CWardee <chriswardee@outlook.com>
2020-02-06 12:13:56 +0000

    CleanedUp/FixNullPtr

--- Assets/Scripts/WordBank.cs
+++ Assets/Scripts/WordBank.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WordBank : MonoBehaviour
 {
+    public TextMeshPro Text;
     public TextAsset wordBank;
     private string[] dataLines;
 
@@ -21,7 +23,10 @@ public class WordBank : MonoBehaviour
         if (timer <= 0.0f)
         {
             timer = 1.0f;
-            Debug.Log(dataLines[Random.Range(0,999)]);
+            int i = Random.Range(0, 999);
+            Debug.Log(dataLines[i]);
+            Text.text = dataLines[i];
+            //Text.text = "Enter Your Text Here";
         }
     }
 }

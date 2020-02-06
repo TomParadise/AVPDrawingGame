81e8100543b40eb301f7337eb1aa831b12f579b6
m <m>
2020-02-06 14:56:33 +0000

    Added a bunch of the stuff in and it works now
    - Draw script works
    - Word bank script works + textmesh pro
    - Imported models and textures

---
+++ Draw It!/Assets/Scripts/WordBank.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WordBank : MonoBehaviour
+{
+    public TextMeshPro Text;
+    public TextAsset wordBank;
+    private string[] dataLines;
+
+    private float timer = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dataLines = wordBank.text.Split('\n');
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0.0f)
+        {
+            timer = 1.0f;
+            int i = Random.Range(0, 850);
+            Debug.Log(dataLines[i]);
+            Text.text = dataLines[i];
+            //Text.text = "Enter Your Text Here";
+        }
+    }
+}
